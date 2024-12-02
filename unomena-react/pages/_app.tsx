3c8c8c1d5f1e9e708b57837fe223a469d9cb72a1
Madele Theron <Madele.theron@gmail.com>
2024-12-02 14:38:51 +0200

    Small Fixes
    - eslint: Disable the react/jsx-key rule
    - remove unused font family (Lora)
    - add .vercel to gitignore

--- unomena-react/pages/_app.tsx
+++ unomena-react/pages/_app.tsx
@@ -1,8 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Lora, Noto_Sans } from 'next/font/google'
+import { Noto_Sans } from 'next/font/google'
 
-const lora = Lora({ subsets: ['latin']});
 const notoSans = Noto_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
