8de2c65c0e3f28735db22678e5d486a397d8dcfd
Madele Theron <Madele.theron@gmail.com>
2024-12-02 15:12:59 +0200

    Add website title and description

--- unomena-react/pages/_app.tsx
+++ unomena-react/pages/_app.tsx
@@ -1,13 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Noto_Sans } from 'next/font/google'
+import Head from 'next/head';
+import { Noto_Sans } from 'next/font/google';
 
 const notoSans = Noto_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={notoSans.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+          <title>Popstar Ventures</title>
+          <meta name="description" content="Creative VC For The Creator Economy" />
+          <meta name="viewport" content="width=1440px, initial-scale=1" />
+        </Head>
+      <main className={notoSans.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   ) 
 }
